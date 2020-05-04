4be98ac5a912e5db32d3c6277a790c3c316bab88
Gyula Kiri <38438001+duszmox@users.noreply.github.com>
2020-05-04 09:44:32 +0200

    config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/guaman_base/';
+$config['base_url'] = 'https://guamanpj.eu/project-elena';
 
 /*
 |--------------------------------------------------------------------------
--- application/config/guaman_config.php
+++ application/config/guaman_config.php
@@ -3,4 +3,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $config["table_prefix"] = "pm_";
 $config["language_array"] = array("hungarian","english");
-$config["project_title"] = "Guaman Base";
+$config["project_title"] = "Project-elena";
